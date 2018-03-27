247ae6f2a66fd445e9bbaee793e25ec4a58dcb68
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-03-27 14:45:36 -0500

    finished onject exercise

--- js/circle.js
+++ js/circle.js
@@ -5,35 +5,66 @@
     var circle = {
         radius: 3,
 
-    function getArea(radius) {
 
-    return  Math.PI * (Math.pow(radius, 2));
-     }
 
-    document.write(getArea(420));
+        getDiameter: function () {
+            return this.radius * 2;
+        },
+        getArea: function () {
+            return  Math.PI * (Math.pow(this.radius, 2))
+        },
 
-    //     logInfo: function (doRounding) {
-    //         // TODO: complete this method.
-    //
-    //         // If doRounding is true, round the result to the nearest integer.
-    //         // Otherwise, output the complete value
-    //
-    //         console.log("Area of a circle with radius: " + this.radius + ", is: ");
-    //     }
-    // };
-    //
-    // // log info about the circle
-    // console.log("Raw circle information");
-    // circle.logInfo(false);
-    // console.log("Circle information rounded to the nearest whole number");
-    // circle.logInfo(true);
-    //
-    // console.log("=======================================================");
-    // // TODO: Change the radius of the circle to 5.
-    //
-    // // log info about the circle
-    // console.log("Raw circle information");
-    // circle.logInfo(false);
-    // console.log("Circle information rounded to the nearest whole number");
-    // circle.logInfo(true);
+
+
+
+
+
+//******* console.log(circle);
+//
+
+     //    getArea: function (radius) {
+     //    while (isNaN(radius)) {
+     //       radius = prompt("that's not a number,  gimme a number")
+     //    }
+     //    return "the area of your circle " + Math.PI * (Math.pow(radius, 2));
+     // },
+
+//*********prompt(getArea());
+//
+// *****document.write(getArea(prompt("gimme a number")));
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            if (doRounding === true){
+               console.log(Math.round(circle.getArea()/* + " " + circle.getDiameter()*/));
+            } else {
+                console.log("Area of a circle with radius: " + this.radius + ", is: ");
+
+            }
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+        }
+
+    };
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5,
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+
 })();
