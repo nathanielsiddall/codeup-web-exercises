ef5762ec5d9fa174aa458c193b62a419c9044944
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-04-03 09:33:44 -0500

    working on the map API exercise

---
+++ js/google-api.js
@@ -0,0 +1,17 @@
+
+
+(function () {
+
+var mapOptions = {
+
+    zoom:19,
+
+    center: {
+        lat:29.426791,
+        lng: -98.489602
+    }
+};
+
+var map = new googgle.maps.map(document.getElementById("map-canvas"), mapOptions);
+
+})();
