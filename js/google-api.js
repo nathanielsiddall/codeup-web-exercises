29d19b43ef108f08bb474879160e057c7cf83ece
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-04-05 14:21:07 -0500

    first commit on jquery exercise

--- js/google-api.js
+++ js/google-api.js
@@ -1,25 +1,47 @@
-
+var loc = { lat: 29.429447, lng: -98.491623 };
 
 (function () {
 
-    var loc = { lat: 29.429447, lng: -98.491623 };
-var mapOptions = {
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-    zoom:15,
-    center: loc
-};
 
+    var address = prompt("gimme an address");
+
+    var geocoder = new google.maps.Geocoder();
+
+    var infoWindowContent = prompt("What do you want to say about this?");
+
+
+
+    geocoder.geocode({ "address": address }, function(results, status) {
+
+        if (status === google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map
+            });
+
+            var infoWindow = new google.maps.InfoWindow({
+                content: infoWindowContent
+            });
+            infoWindow.open(map, marker);
+
+            loc = results[0].geometry.location;
+        } else {
+            alert("Geocoding was not successful - STATUS: " + status);
+        }
+    });
+
+
+    var mapOptions = {
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        zoom:15
+
+    };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-    var marker = new google.maps.Marker({
-        position: loc,
-        map: map
-    });
 
-    var infoWindow = new google.maps.InfoWindow({
-        content: "your face is dumb and your code is too."
-    });
 
-    infoWindow.open(map, marker);
+loc.log
 
 })();
+
