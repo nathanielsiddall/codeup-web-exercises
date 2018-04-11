241df5b6557686fe081a6b45452ed4e5792ef58a
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-04-11 11:34:13 -0500

    working on getting the map set up on the weather api. i'm saving because i think i messed up. if i didnt great.

--- js/google-api.js
+++ js/google-api.js
@@ -3,45 +3,53 @@ var loc = { lat: 29.429447, lng: -98.491623 };
 (function () {
 
 
-    var address = prompt("gimme an address");
+    // ?var address = prompt("gimme an address");
 
-    var geocoder = new google.maps.Geocoder();
+    // var geocoder = new google.maps.Geocoder();
 
-    var infoWindowContent = prompt("What do you want to say about this?");
+    // var infoWindowContent = prompt("What do you want to say about this?");
 
 
 
-    geocoder.geocode({ "address": address }, function(results, status) {
+    // geocoder.geocode({ "address": loc }, function(results, status) {
+    //
+    //     if (status === google.maps.GeocoderStatus.OK) {
+    //         map.setCenter(results[0].geometry.location);
+    //         var marker = new google.maps.Marker({
+    //             position: results[0].geometry.location,
+    //             map: map,
+    //             draggable: true
+    //         });
+    //
+    //         var infoWindow = new google.maps.InfoWindow({
+    //             content: infoWindowContent
+    //         });
+    //         infoWindow.open(map, marker);
+    //
+    //         loc = results[0].geometry.location;
+    //     } else {
+    //         alert("Geocoding was not successful - STATUS: " + status);
+    //     }
+    // });
 
-        if (status === google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                position: results[0].geometry.location,
-                map: map
-            });
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
 
-            var infoWindow = new google.maps.InfoWindow({
-                content: infoWindowContent
-            });
-            infoWindow.open(map, marker);
+        {
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            zoom: 5,
+            center: loc
 
-            loc = results[0].geometry.location;
-        } else {
-            alert("Geocoding was not successful - STATUS: " + status);
-        }
+        });
+
+    var marker = new google.maps.Marker({
+        position: loc,
+        draggable: true,
+        map: map
+
     });
 
 
-    var mapOptions = {
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        zoom:15
-
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-
-
-loc.log
+    console.log(marker);
 
 })();
 
